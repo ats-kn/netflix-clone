da37f9d61156c32e48d23e0da65e8aa9f430fcd8
ats <atsushi.k214@gmail.com>
2024-06-30 17:05:02 +0900

    コメント付与

--- src/components/Row/Layout.tsx
+++ src/components/Row/Layout.tsx
@@ -1,6 +1,11 @@
+/*
+Layout.tsx：コンポーネントのUI部分を定義
+propsとして受け取った値を元に，Rowコンポーネントの表示が責務
+*/
 import { Movie } from "../../type.ts";
 import YouTube from "react-youtube";
 
+// Props：コンポーネントに渡す引数の形式を定義
 export type Props = {
   title: string;
   fetchUrl: string;
@@ -44,8 +49,10 @@ export const Layout = ({
       <h2>{title}</h2>
       <div className="flex overflow-y-hidden overflow-x-scroll p-5 scrollbar-hide">
         {movies.map((movie) => (
+          // DOM表示にmapを使う際はkeyを指定する
           <img
             key={movie.id}
+            // 使用する画像を使い分ける
             className={`object-contain w-full max-h-24 m-2 transform transition-transform duration-450 ${
               isLargeRow ? "max-h-60 hover:scale-110" : "hover:scale-108"
             }`}
