ccd5065d5106e7854e8eac55ef554faf6404909f
ats <atsushi.k214@gmail.com>
2024-06-30 16:25:42 +0900

    コメント付与

--- src/components/Row/useProps.ts
+++ src/components/Row/useProps.ts
@@ -1,3 +1,6 @@
+/*
+useProps.ts：コンポーネントのロジック部分を定義
+*/
 import { useEffect, useState } from "react";
 import axios from "../../axios";
 import { Movie } from "../../type.ts";
@@ -6,16 +9,18 @@ import { requests } from "../../request.ts";
 export const useProps = (fetchUrl: string) => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [trailerUrl, setTrailerUrl] = useState<string | null>("");
-
+  // APIからデータを取得するのにuseEffectを使用
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
+      // Movie型に合わせてrequest.data.resultsの配列をmapで抽出して整形
       const movies = request.data.results.map((movie: Movie) => ({
         id: movie.id,
         name: movie.name,
         poster_path: movie.poster_path,
         backdrop_path: movie.backdrop_path,
       }));
+
       setMovies(movies);
       return request;
     }
