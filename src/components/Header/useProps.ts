78a6433d7844799e6bee4cca4de024454e295432
ats <atsushi.k214@gmail.com>
2024-07-01 16:53:26 +0900

    コメント付与

--- src/components/Header/useProps.ts
+++ src/components/Header/useProps.ts
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+// Headerコンポーネントの背景をスクロール量に応じて変更するためのフック
 export const useProps = () => {
   const [show, setShow] = useState(false);
   useEffect(() => {
+    // スクロール量が100を超えたらshowをtrueにする
     const handleShow = () => {
       setShow(window.scrollY > 100);
     };
 
+    // イベントリスナの設定
     window.addEventListener("scroll", handleShow);
     return () => {
       window.removeEventListener("scroll", handleShow);
