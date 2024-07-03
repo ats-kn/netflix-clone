b698dea554ddefc9660a6c3f864016a176f3d355
ats <atsushi.k214@gmail.com>
2024-07-04 00:27:57 +0900

    BannerDataProviderで全体をラップ

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Row } from "./components/Row";
 import { Banner } from "./components/Banner";
 import { Header } from "./components/Header";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { BannerDataProvider } from "./BannerDataContext";
 
 // react-queryを定義
 const queryClient = new QueryClient();
@@ -11,21 +12,23 @@ function App() {
   return (
     // react-queryを使用するためにQueryClientProviderで囲む
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Header />
-        <Banner />
-        <Row
-          title="NETFLIX ORIGUINALS"
-          fetchUrl={requests.fetchNetflixOriginals}
-          isLargeRow
-        />
-        <Row title="Trand Movies" fetchUrl={requests.fetchTrending} />
-        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-        <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} />
-        <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} />
-        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-        <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
-      </div>
+      {/* 定義したBannerDataProviderでラップする */}
+      <BannerDataProvider>
+        <div className="App">
+          <Header />
+          <Banner />
+          <Row
+            title="NETFLIX ORIGUINALS"
+            fetchUrl={requests.fetchNetflixOriginals}
+          />
+          <Row title="Trand Movies" fetchUrl={requests.fetchTrending} />
+          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+          <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} />
+          <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} />
+          <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+          <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
+        </div>
+      </BannerDataProvider>
     </QueryClientProvider>
   );
 }
