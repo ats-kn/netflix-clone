27b870b355c8bc0a3d571f95fd1b5cb58e7725dc
ats <atsushi.k214@gmail.com>
2024-07-04 11:54:50 +0900

    rowコンポーネントに表示されるサムネイルの画像がnullのものをfilterで省く

--- src/components/Row/useProps.ts
+++ src/components/Row/useProps.ts
@@ -16,13 +16,17 @@ export const useProps = (fetchUrl: string, title: string) => {
 
     // 取得するデータを10件に制限
     // 配列に対してsliceを使って10件に制限
-    return request.data.results.slice(0, 10).map((movie: Movie) => ({
-      id: movie.id,
-      name: movie.name,
-      poster_path: movie.poster_path,
-      backdrop_path: movie.backdrop_path,
-      overview: movie.overview,
-    }));
+    // サムネイル画像が存在するデータのみをフィルタリング
+    return request.data.results
+      .slice(0, 10)
+      .filter((movie: Movie) => movie.backdrop_path != null)
+      .map((movie: Movie) => ({
+        id: movie.id,
+        name: movie.name,
+        poster_path: movie.poster_path,
+        backdrop_path: movie.backdrop_path,
+        overview: movie.overview,
+      }));
   };
   //react-queryを使ってfetchDataを実行
   // useQueryでデータが取得できたらmoviesに格納，データ取得中はisLoadingをtrueにする
